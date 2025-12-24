089d28c327dafef135a8f967153fa2cf0580aa8e
Reiwii <oliwierrgorz@gmail.com>
2025-12-24 19:59:15 +0100

    fixed projectile pierce

--- Projectile.py
+++ Projectile.py
@@ -8,17 +8,32 @@ class Projectile(pygame.sprite.Sprite):
         self.damage = damage
         self.pierce = pierce
         self.owner = owner
+        self.radius = 3
+        self.pierce = 2
+        self.enemies_hit: set[pygame.sprite.Sprite] = set()
+        self.lifetime = 1.0
+        self.age = 0
 
         self.image = pygame.Surface((8, 8), pygame.SRCALPHA)
         self.image.fill((255, 255, 0))
         self.rect = self.image.get_rect(center=self.pos)
 
     def update(self, world, dt):
+        self.age += dt
+        if self.age > self.lifetime:
+            self.kill()
         self.pos += self.vel * dt
         self.rect.center = (round(self.pos.x), round(self.pos.y))
 
-        hits = pygame.sprite.spritecollide(self, world.enemies_group, False)
+        hits = pygame.sprite.spritecollide(self, world.enemies_group, False,collided=pygame.sprite.collide_circle)
+
         for e in hits:
-            e.take_damage(self.damage)
-            self.kill()
+            if e in self.enemies_hit:
+                continue
+            self.enemies_hit.add(e)
+            e.take_damage(self.damage,world)
+            self.pierce -= 1
+            if self.pierce == 0:
+                self.kill()
+                break
 
